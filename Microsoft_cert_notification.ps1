388485e55f0e1acacce1b0667cf4391f3ce68904
LeruKenny <113237255+LeruKenny@users.noreply.github.com>
2023-02-20 21:55:16 +0100

    Update Microsoft_cert_notification.ps1

--- Microsoft_cert_notification.ps1
+++ Microsoft_cert_notification.ps1
@@ -1,24 +1,20 @@
-$caName = ""
-
-
-$certTemplates = Get-CATemplate -CertificationAuthority $caName
+# Gets the contents of the certificates on the machine based on the file path
+$cert = Get-ChildItem -Path Cert:\LocalMachine -Recurse | Where-Object {$_.Subject -like "*Fedramp"}
 $result = @()
 
-foreach ($certTemplate in $certTemplates) {
-
-    # Get the certificates issued from the certificate template
-    $certs = Get-ChildItem -Path cert:\LocalMachine -ExpiringInDays 30 | Where-Object { $_.Subject -match "$($certTemplate.Name)" }
-
-    # Loop through each certificate
-    foreach ($cert in $certs) {
-
-        $result += New-Object -TypeName PSObject -Property ([ordered]@{
-            'Certificate' = $cert.Name;
-            'Expires'     = $cert.NotAfter
- 
-        })
+# Loops through the certificates to check for Certificate less than 30 days to expiration
+foreach ($c in $cert) {
+    $timeLeft = ($c.NotAfter - (Get-Date).Date).Days
+    if ($timeLeft -ge 0 -and $timeLeft -lt 2000 ){
+        # Collects all the certificate name and expiration date into result 
+        $result+=New-Object -TypeName PSObject -Property ([ordered]@{
+                     'Certificate'=$c.Issuer;
+                     'Expiry Date'=$c.NotAfter
+        }) 
     }
-    
-}
-Write-Output $result 
-$result | Export-Csv -Path "C:\Users\\Desktop\expired_certificate.csv" -Append -NoTypeInformation
+}    
+
+Write-Output $result
+$File_name = Get-Date -Format "yyyy-MM-dd"
+# Exports the content of the result into a Csv file
+$result | Export-Csv -Path "C:\Users\Leru Kenny\Desktop\Expired_cert_$($File_name).csv" -Append -NoTypeInformation
