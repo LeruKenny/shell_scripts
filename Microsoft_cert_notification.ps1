28b67e0d56c9e4e8eee9d499525741635c96fba7
LeruKenny <113237255+LeruKenny@users.noreply.github.com>
2023-02-20 14:29:41 +0100

    Update Microsoft_cert_notification.ps1
    made changes to the code

--- Microsoft_cert_notification.ps1
+++ Microsoft_cert_notification.ps1
@@ -1,16 +1,14 @@
-$result=@() 
+$result=@()  
+# $ErrorActionPreference="SilentlyContinue"
  
-$ErrorActionPreference="SilentlyContinue"
- 
-$a=Invoke-Command {Get-ChildItem Cert:\LocalMachine\ -Recurse |
-Where-Object {$_ -is [System.Security.Cryptography.X509Certificates.X509Certificate2] -and $_.NotAfter -gt (Get-Date) -and $_.NotAfter -lt (Get-Date).AddDays(30)} }
+$a = Get-ChildItem Cert:\LocalMachine -Recurse -ExpiringInDays 2000 
  
 foreach ($c in $a) {
 $result+=New-Object -TypeName PSObject -Property ([ordered]@{
-'Server'=$i;
 'Certificate'=$c.Issuer;
 'Expires'=$c.NotAfter 
 })
  
 } 
 Write-Output $result
+$result | Export-Csv -Path "C:\Users\\Desktop\certs_to_expire.csv" -Append -NoTypeInformation
