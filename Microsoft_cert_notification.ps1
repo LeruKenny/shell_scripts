647af88955cf06aa4cc8adea947277bedb4332f6
LeruKenny <113237255+LeruKenny@users.noreply.github.com>
2023-02-20 17:00:10 +0100

    Update Microsoft_cert_notification.ps1

--- Microsoft_cert_notification.ps1
+++ Microsoft_cert_notification.ps1
@@ -1,15 +1,24 @@
-$OutputPath = "C:\Users\\Desktop\certs_to_expire.csv"
-$result=@()  
-# $ErrorActionPreference="SilentlyContinue"
+$caName = "fedramp-preprod-ca"
+
+
+$certTemplates = Get-CATemplate -CertificationAuthority $caName
+$result = @()
+
+foreach ($certTemplate in $certTemplates) {
+
+    # Get the certificates issued from the certificate template
+    $certs = Get-ChildItem -Path cert:\LocalMachine -ExpiringInDays 30 | Where-Object { $_.Subject -match "$($certTemplate.Name)" }
+
+    # Loop through each certificate
+    foreach ($cert in $certs) {
+
+        $result += New-Object -TypeName PSObject -Property ([ordered]@{
+            'Certificate' = $cert.Name;
+            'Expires'     = $cert.NotAfter
  
-$a = Get-ChildItem Cert:\LocalMachine -Recurse -ExpiringInDays 2000 
- 
-foreach ($c in $a) {
-$result+=New-Object -TypeName PSObject -Property ([ordered]@{
-'Certificate'=$c.Issuer;
-'Expires'=$c.NotAfter 
-})
- 
-} 
-Write-Output $result
-$result | Export-Csv -Path $OutputPath -Append -NoTypeInformation
+        })
+    }
+    
+}
+Write-Output $result 
+$result | Export-Csv -Path "C:\Users\oajayi.tlx\Desktop\expired_certificate.csv" -Append -NoTypeInformation
